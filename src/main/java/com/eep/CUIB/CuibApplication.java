fa21d6862ba004cf91045724357888130a150689
Carlos <robocarlos10@gmail.com>
2022-02-04 10:24:14 +0100

    Inicio de Repositorio CUIB

---
+++ src/main/java/com/eep/CUIB/CuibApplication.java
@@ -0,0 +1,13 @@
+package com.eep.CUIB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eep/CUIB/CuibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eep.CUIB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
